eb2986c94acbbaad57a4858c251f2893c3c75b3b
szukeymaker <szukeymaker@gmail.com>
2025-04-04 01:15:40 +0800

    ADD test_asyncscope.cc

---
+++ test_asyncscope.cc
@@ -0,0 +1,42 @@
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <folly/init/Init.h>
+#include <folly/coro/AsyncScope.h>
+#include <folly/executors/GlobalExecutor.h>
+#include <folly/coro/Task.h>
+#include <folly/coro/BlockingWait.h>
+
+folly::coro::Task<void> test() {
+  std::atomic<int> count = 0;
+
+  auto incrementBy5 = [&]() -> folly::coro::Task<> {
+    count += 5;
+    co_return;
+  };
+
+  auto incrementBy10 = [&]() -> folly::coro::Task<> {
+    count += 10;
+    co_return;
+  };
+
+  auto incrementBy100 = [&]() -> folly::coro::Task<> {
+    count += 100;
+    co_return;
+  };
+
+  folly::coro::AsyncScope scope;
+  scope.add(incrementBy5().scheduleOn(folly::getGlobalCPUExecutor()));
+  scope.add(incrementBy10().scheduleOn(folly::getGlobalCPUExecutor()));
+  scope.add(incrementBy100().scheduleOn(folly::getGlobalCPUExecutor()));
+
+  co_await scope.joinAsync();
+  std::cout << "count:" << count << std::endl;
+  co_return;
+}
+
+int main(int argc, char** argv) {
+  folly::Init init(&argc, &argv);
+  folly::coro::blockingWait(test());
+  return 0;
+}
